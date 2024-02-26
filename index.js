eec73ead81a8e8710d74151523a6a9f7e7d82a36
D.V.Kavya Sai <d.v.kavyasai@gmail.com>
2024-02-26 21:14:08 +0530

    B's work over, A takes up

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
-app.listen(port, () => {
-    console.log(`API is running at http://localhost:${port}`);
-});
 
 app.get("/",(req,res)=>{
     res.render("index.ejs");
+});
+
+app.get("/history",(req,res)=>{
+    res.render("history.ejs");
+});
+
+app.listen(port, () => {
+    console.log(`API is running at http://localhost:${port}`);
 });
